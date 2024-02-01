7aa33ec1406154f75f95582e0a38259f65c9b2bf
Stelse1337 <122170451+Stelse1337@users.noreply.github.com>
2024-02-01 17:19:15 +0300

    Add files via upload

---
+++ 76/test.js
@@ -0,0 +1,11 @@
+const firstName = 'Slava';
+const surname = 'Fomin';
+const patr = 'Alekseevich';
+let age = 24;
+let height = 188;
+
+console.log(firstName);
+console.log(surname);
+console.log(patr);
+console.log(age);
+console.log(height);
