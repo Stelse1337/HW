5ccf9e96bbd2ad5ae7a32eab79d6e728c4d048ea
Stelse1337 <122170451+Stelse1337@users.noreply.github.com>
2024-02-02 12:09:15 +0300

    Add files via upload

---
+++ 80/test.js
@@ -0,0 +1,23 @@
+console.log('Выведите в консоль четные числа от 2 до 12, используйте цикл for и условие.');
+for(let i = 2; i <= 12; i = i+2) {
+    console.log(i);
+}
+
+console.log('При помощи цикла for выведите в консоль числа от 10 до 1, в обратном порядке.');
+for(let y = 10; y > 0; y--) {
+    console.log(y);
+}
+
+console.log('Перепишите 1ый цикл используя цикл while.');
+let x = 2;
+while(x <= 12) {
+    console.log(x);
+    x = x + 2;
+}
+
+console.log('Перепишите 2ой цикл используя цикл while.');
+let z = 10;
+while(z > 0) {
+    console.log(z);
+    z--;
+}
