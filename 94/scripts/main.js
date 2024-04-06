906443e171b03b7b17a933f6f0b06f142ae8455a
Stelse1337 <122170451+Stelse1337@users.noreply.github.com>
2024-04-06 07:33:37 +0300

    Add files via upload

--- 94/scripts/main.js
+++ 94/scripts/main.js
@@ -6,3 +6,15 @@ burgerIcon.addEventListener('click', () => {
 		document.body.classList.remove('body--opened-menu');
 	}
 });
+
+
+const modal = document.querySelector('.modal');
+const gift = document.querySelector('.about__img-button');
+gift.addEventListener('click', () => {
+	modal.style.display = 'flex';
+});
+
+const modalCancel = document.querySelector('.modal__cancel');
+modalCancel.addEventListener('click', () => {
+	modal.style.display = 'none';
+});
