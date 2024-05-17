c2263c2a310fc520843ae3836bb695d66be69914
Stelse1337 <122170451+Stelse1337@users.noreply.github.com>
2024-05-17 18:03:44 +0300

    Add files via upload

--- 94/scripts/main.js
+++ 94/scripts/main.js
@@ -1,99 +1,169 @@
-// Бургер
-const burgerIcon = document.querySelector('.burger-icon');
-burgerIcon.addEventListener('click', () => {
-	if (document.body.className == 'body') {
-		document.body.classList.add('body--opened-menu');
-	} else {
-		document.body.classList.remove('body--opened-menu');
-	}
-});
+(function () {
 
+	// Бургер
 
-// Модалка
-const modal = document.querySelector('.modal');
-const gift = document.querySelector('.about__img-button');
-gift.addEventListener('click', () => {
-	modal.style.display = 'flex';
-});
+	document.addEventListener('click', burgerInit)
 
-const modalCancel = document.querySelector('.modal__cancel');
-modalCancel.addEventListener('click', () => {
-	modal.style.display = 'none';
-});
+	function burgerInit(e) {
 
+		const burgerIcon = e.target.closest('.burger-icon')
+		const burgerNavLink = e.target.closest('.nav__link')
 
-// Табы
-const tabControls = document.querySelector('.tab-controls');
-tabControls.addEventListener('click', toggleTab);
-function toggleTab(e) {
-	const tabButton = e.target.closest('.tab-controls__link');
-	if (!tabButton) return;
-	e.preventDefault();
-	if (tabButton.classList.contains('tab-controls__link--active')) return;
+		if (!burgerIcon && !burgerNavLink) return
+		if (document.documentElement.clientWidth > 900) return
 
-	const tabContentId = tabButton.getAttribute('href');
-	const tabContent = document.querySelector(tabContentId);
-	const activeButton = document.querySelector('.tab-controls__link--active');
-	const activeContent = document.querySelector('.tab-content--show');
-
-	tabContent.classList.add('tab-content--show');
-	tabButton.classList.add('tab-controls__link--active');
-
-	activeContent.classList.remove('tab-content--show');
-	activeButton.classList.remove('tab-controls__link--active');
-}
-
-// Аккордеон
-const accordionLists = document.querySelectorAll('.accordion-list');
-accordionLists.forEach(el => {
-	el.addEventListener('click', (e) => {
-		const accordionList = e.currentTarget;
-		const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened');
-		const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content');
-
-		const accordionControl = e.target.closest('.accordion-list__control');
-		if (!accordionControl) return;
-		const accordionItem = accordionControl.parentElement;
-		const accordionContent = accordionControl.nextElementSibling;
-
-		if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
-			accordionOpenedItem.classList.remove('accordion-list__item--opened');
-			accordionOpenedContent.style.maxHeight = null;
-		}
-		accordionItem.classList.toggle('accordion-list__item--opened');
-
-		if (accordionItem.classList.contains('accordion-list__item--opened')) {
-			accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+		if (!document.body.classList.contains('body--opened-menu')) {
+			document.body.classList.add('body--opened-menu')
 		} else {
-			accordionContent.style.maxHeight = null;
+			document.body.classList.remove('body--opened-menu')
+		}
+
+	}
+
+	// Модалка
+
+	const modal = document.querySelector('.modal')
+	const modalButton = document.querySelector('.about__img-button')
+
+	modalButton.addEventListener('click', openModal)
+	modal.addEventListener('click', closeModal)
+
+	function openModal(e) {
+		e.preventDefault()
+		document.body.classList.toggle('body--opened-modal')
+	}
+
+	function closeModal(e) {
+		e.preventDefault()
+
+		const target = e.target
+
+		if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+			document.body.classList.remove('body--opened-modal')
+		}
+
+	}
+
+	// Табы
+
+	const tabControls = document.querySelector('.tab-conrols')
+
+	tabControls.addEventListener('click', toggleTab)
+
+	function toggleTab(e) {
+
+		const tabControl = e.target.closest('.tab-conrols__link')
+
+		if (!tabControl) return
+		e.preventDefault()
+		if (tabControl.classList.contains('tab-conrols__link--active')) return
+
+		const tabContentID = tabControl.getAttribute('href')
+		const tabContent = document.querySelector(tabContentID)
+		const activeControl = document.querySelector('.tab-conrols__link--active')
+		const activeContent = document.querySelector('.tab-content--show')
+
+		if (activeControl) {
+			activeControl.classList.remove('tab-conrols__link--active')
+		}
+		if (activeContent) {
+			activeContent.classList.remove('tab-content--show')
+		}
+
+		tabControl.classList.add('tab-conrols__link--active')
+		tabContent.classList.add('tab-content--show')
+
+	}
+
+	// Аккордеон
+
+	const accordionLists = document.querySelectorAll('.accordion-list');
+
+	accordionLists.forEach(el => {
+
+		el.addEventListener('click', (e) => {
+
+			const accordionList = e.currentTarget
+			const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+			const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+			const accordionControl = e.target.closest('.accordion-list__control');
+			if (!accordionControl) return
+			const accordionItem = accordionControl.parentElement;
+			const accordionContent = accordionControl.nextElementSibling;
+
+			if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+				accordionOpenedItem.classList.remove('accordion-list__item--opened');
+				accordionOpenedContent.style.maxHeight = null;
+			}
+			accordionItem.classList.toggle('accordion-list__item--opened');
+
+			if (accordionItem.classList.contains('accordion-list__item--opened')) {
+				accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+			} else {
+				accordionContent.style.maxHeight = null;
+			}
+
+		});
+
+	});
+
+	// Слайдер-галерея
+
+	new Swiper('.gallery__slider', {
+
+		spaceBetween: 15,
+		slidesPerView: 1.5,
+
+		pagination: {
+			el: '.gallery__pagination',
+			type: 'fraction',
+		},
+
+		navigation: {
+			nextEl: '.gallery__next',
+			prevEl: '.gallery__prev',
+		},
+
+		breakpoints: {
+			601: {
+				slidesPerView: 3,
+			},
+			801: {
+				spaceBetween: 32,
+			},
+			1101: {
+				slidesPerView: 4,
+			}
 		}
 	});
-});
 
-// Слайдер-галерея
-const swiper = new Swiper('.gallery__slider', {
-	spaceBetween: 15,
-	slidesPerView: 1.5,
-  pagination: {
-    el: '.gallery__pagination',
-		type: 'fraction'
-  },
-  navigation: {
-    nextEl: '.gallery__next',
-    prevEl: '.gallery__prev'
-  },
-	breakpoints: {
-		321: {
-			slidesPerView: 2
+	// Слайдер-отзывы
+
+	new Swiper('.testimonials__slider', {
+
+		spaceBetween: 0,
+		slidesPerView: 1,
+		centeredSlides: true,
+
+		navigation: {
+			nextEl: '.testimonials__next',
+			prevEl: '.testimonials__prev',
 		},
-		601: {
-			slidesPerView: 3
+
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true,
 		},
-		801: {
-			spaceBetween: 32
-		},
-    1101: {
-      slidesPerView: 4
-    }
-  }
-});
+
+		breakpoints: {
+			901: {
+				slidesPerView: 1.5
+			},
+			1201: {
+				slidesPerView: 2.1
+			}
+		}
+	});
+
+})()
